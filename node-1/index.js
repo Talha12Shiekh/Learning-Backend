71951f86ee62ae86fdb60460460f109b74c8e01c
Talha Khurram <tk.shiekh4567@gmail.com>
2025-05-13 04:42:25 -0700

    Learning Concepts of server

---
+++ node-1/index.js
@@ -0,0 +1,46 @@
+// import { sum, diff } from "./lib.js";
+// const { sum, diff } = require("./lib");
+
+// const fs = require("fs");
+// const express = require("express");
+const os = require("os");
+
+// const t1 = performance.now();
+
+// // const text = fs.readFileSync("demo.txt", "utf-8");
+// fs.readFile("demo.txt", "utf-8", (err, txt) => {
+//   console.log(txt);
+// });
+
+// // console.log(text);
+// const t2 = performance.now();
+
+// console.log(sum(4, 5), diff(9, 5));
+
+// console.log("Time taken: ", t2 - t1);
+
+// console.log("Hello");
+
+// const server = express();
+// server.listen(8080);
+
+// console.log(os.userInfo());
+
+// const { exec } = require("child_process");
+// const fs = require("fs");
+
+// exec("dir", (er, stdout, stderr) => {
+//   if (er) {
+//     console.log(`Error executing command ${er}`);
+//     return;
+//   }
+//   fs.writeFile("demo.txt", stdout, (er) => {
+//     if (er) {
+//       console.log(er);
+//     } else {
+//       console.log("File have been written");
+//     }
+//   });
+//   console.log(`Standard output: ${stdout}`);
+//   console.log(`Standard error: ${stderr}`);
+// });
