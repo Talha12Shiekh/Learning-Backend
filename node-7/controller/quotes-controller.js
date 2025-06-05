28413faf3d9caf660486d8661c03293e23c53929
Talha Khurram <tk.shiekh4567@gmail.com>
2025-06-05 05:01:08 -0700

    Deploying my MERN application

--- node-7/controller/quotes-controller.js
+++ node-7/controller/quotes-controller.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const path = require("path");
-const data = JSON.parse(fs.readFileSync(path.resolve(__dirname,"quotes.json"), "utf-8"));
+const data = JSON.parse(fs.readFileSync(path.resolve(__dirname, "../quotes.json"), "utf-8"));
 const quotes = data.quotes;
 
 exports.getAllQuotes = (req, res) => {
